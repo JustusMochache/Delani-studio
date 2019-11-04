7334d6684f04eb686a8cb2fac605c304272c9098
Justusm10 <justusm10@gmail.com>
2019-11-04 07:16:31 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# Delani Studio
+
+This is a landing page for Akan name generator
+
+## Table of content
+
+---
+
+### Application Descrption
+
+Delani Studio is a site built to practice concepts learnt including JQuery, bootstrap, javascript and working with APIS's.
+
+---
+
+### Delani studio Form
+
+---
+
+### Delani studio portfolio
+
+---
+
+## Bugs.
+
+The MailChimp API doesn't work properly.
+
+---
+
+## Technologies
+
+#### html
+
+#### css
+
+#### javascript
+
+#### jquery
+
+---
+
+## BDD
+
+<img src="./images/delani.png">
+
+| Inputs  |                              Description |
+| :------ | ---------------------------------------: |
+| Name    |     The users name. `eg Justus onyancha` |
+| Email   | The users email. `eg usermail@gmail.com` |
+| Message |               The message from the user. |
+
+the general behaviours/functionality of the project is that its intuitive because its direct and doesnt have to open in a new tab.
+This website was designed to have little information but just the application to avoid clutter as a design principle..
+
+---
+
+## license
+
+Our License is for public use and application can be copied.
+
+---
+
+### MIT license
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so.
+
+---
+
+## Author
+
+Website:
+
+Twitter: @justusmo
+
+---
+
+## Contact
+
+0714244366
+
+email: justusm10@gmail.com
---
+++ js/script.js
@@ -0,0 +1,68 @@
+$(function() {
+  let firstext = $("#firstext");
+  let developertext = $("#developertext");
+  let pro = $("#pro");
+  let destext = $("#designtext");
+  let devtext = $("#devtext");
+  let protext = $("#protext");
+
+  $(firstext).hide();
+  $(developertext).hide();
+  $(pro).hide();
+
+  $(".des").on("click", function() {
+    $(".des").slideUp("slow");
+    $(firstext).show(500);
+    $(destext).animate({ fontWeight: "bolder", fontSize: "30px" });
+  });
+  $(".dev").on("click", function() {
+    $(".dev").slideUp("slow");
+    $(developertext).show(500);
+    $(devtext).animate({ fontWeight: "bolder", fontSize: "30px" });
+  });
+  $(".pro").on("click", function() {
+    $(".pro").slideUp("slow");
+    $(pro).show(500);
+    $(protext).animate({ fontWeight: "bolder", fontSize: "30px" });
+  });
+
+  $(firstext, destext).on("click", function() {
+    $(".des").slideDown();
+    $(firstext).hide(500);
+    $(destext).animate({ fontWeight: "normal", fontSize: "16px" });
+  });
+  $(developertext, devtext).on("click", function() {
+    $(".dev").slideDown();
+    $(developertext).hide(500);
+    $(devtext).animate({ fontWeight: "normal", fontSize: "16px" });
+  });
+  $(pro, protext).on("click", function() {
+    $(".pro").slideDown();
+    $(pro).hide(500);
+    $(protext).animate({ fontWeight: "normal", fontSize: "16px" });
+  });
+
+  $(".portfoliopic").hover(
+    function() {
+      $(".porttext", this).slideToggle("slow");
+    },
+    function() {
+      $(".porttext", this).slideToggle("slow");
+    }
+  );
+
+  $("form#form").on("submit", function(event) {
+    event.preventDefault();
+    let name = $("input#name").val();
+    let email = $("input#email").val();
+    let message = $("textarea#message").val();
+
+    if ($("input#name").val() && $("input#email").val()) {
+      alert(
+        "Hey " + name + ", Thank you for contacting us, We will get back to you"
+      );
+    } else {
+      alert("Please provide your correct name and email!");
+    }
+  });
+});
